a51a28eb095e10eec30c4b489bad99432d5809cf
logicalmechanism <logical.mechanism@protonmail.com>
2024-04-07 09:39:50 -0700

    removing excess and trying to fix mobile to top button issue

--- update-explorer.sh
+++
@@ -1,5 +0,0 @@
-cd apps
-rm -fr explorer || true
-cd ../explorer-app
-npm run build
-cp -r build ../apps/explorer
--- update-platform-functions.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-doctl serverless deploy platform-functions --remote-build
